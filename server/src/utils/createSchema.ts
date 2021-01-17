31a9e10e82ce37051d0f93a4cdb98c7ecce15f6d
Christopher Cho <chrischo360@gmail.com>
2021-01-17 06:43:01 -0500

    deleted additional client, server, server2

--- server/src/utils/createSchema.ts
+++
@@ -1,7 +0,0 @@
-import { UserResolver } from "../resolvers/user";
-import { buildSchema } from "type-graphql";
-
-export const createSchema = () =>
-  buildSchema({
-    resolvers: [UserResolver],
-  });
