cb14e2e7898b4c5357429218ab17e73eef70edba
Christopher Cho <chrischo360@gmail.com>
2021-01-24 05:54:59 -0500

    changes

--- server3/api/server.ts
+++ server3/api/server.ts
@@ -2,4 +2,8 @@ import { ApolloServer } from "apollo-server";
 import { schema } from "./schema";
 import Context from "./context";
 // import { context, createContext } from "./context";
-export const server = new ApolloServer({ schema, context: Context });
+export const server = new ApolloServer({
+    schema,
+    context: Context,
+    introspection: true,
+});
