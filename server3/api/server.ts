fd67f4ecaf07b8e2c3d47e7f8b807db753c1154b
Christopher Cho <chrischo360@gmail.com>
2021-01-29 09:06:57 -0500

    deleted server3 and cleaned up server

--- server3/api/server.ts
+++
@@ -1,9 +0,0 @@
-import { ApolloServer } from "apollo-server";
-import { schema } from "./schema";
-import Context from "./context";
-// import { context, createContext } from "./context";
-export const server = new ApolloServer({
-    schema,
-    context: Context,
-    introspection: true,
-});
--- server3/src/graphql/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./Page";
-export * from "./User";
-export * from "./Block";
-export * from "./Mutation";
-export * from "./Query";
-export * from "./AuthPayload";
