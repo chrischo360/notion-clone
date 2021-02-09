e0b39d41bfeea51f59d3d55c8417f14f276b2dc6
Christopher Cho <chrischo360@gmail.com>
2021-02-09 11:54:49 -0500

    deleted files

--- web/src/lib/accessToken.ts
+++
@@ -1,9 +0,0 @@
-export let accessToken: string | null | undefined = ""
-
-export const setAccessToken = (token: string | null | undefined) => {
-    accessToken = token
-}
-
-export const getAccessToken = () => {
-    return accessToken
-}
